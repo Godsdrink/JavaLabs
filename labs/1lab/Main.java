64d57eba74a398d99eea1c1e1f84e0cb93ff3a17
Mirinda Raman <romanostash2000@gmail.com>
2018-05-18 01:18:28 +0300

    edited lab1
    Veres comment resolved

--- labs/1lab/Main.java
+++ labs/1lab/Main.java
@@ -4,23 +4,23 @@ public class Main {
 
 	public static void main(String[] args) {
 		// 3 objects
-		TouristTour Egypt = new TouristTour();
-		TouristTour Canada = new TouristTour(40, 50, 60.5, "Name Surname");
-		TouristTour Japan = new TouristTour(70, 80, 90.5, "Name Surname", true);
+		TouristTour egypt = new TouristTour();
+		TouristTour canada = new TouristTour(40, 50, 60.5, "Name Surname");
+		TouristTour japan = new TouristTour(70, 80, 90.5, "Name Surname", true);
 		// data about objects
-		Egypt.resetValues(10, 20, 30.5, "Endy Goover", true);
-		Canada.resetValues(40, 50, 60.5, "Steve Nuke", false);
-		Japan.resetValues(70, 80, 90.5, "Bob Devil", false);
+		egypt.resetValues(10, 20, 30.5, "Endy Goover", true);
+		canada.resetValues(40, 50, 60.5, "Steve Nuke", false);
+		japan.resetValues(70, 80, 90.5, "Bob Devil", false);
 		// output object data
-		System.out.println(Egypt.toString());
-		System.out.println(Canada.toString());
-		System.out.println(Japan.toString());
+		System.out.println(egypt.toString());
+		System.out.println(canada.toString());
+		System.out.println(japan.toString());
 
 		System.out.println("");
 
-		Egypt.printSum();
-		Canada.printSum();
-		Japan.printSum();
+		egypt.printSum();
+		canada.printSum();
+		japan.printSum();
 
 		System.out.println("");
 
--- labs/1lab/TouristTour.java
+++ labs/1lab/TouristTour.java
@@ -4,36 +4,36 @@ public class TouristTour {
 	private int discount;
 	private int quantity;
 	private double price;
-	private String NameOfTourist;
+	private String nameOfTourist;
 	private boolean vip = false;
-	static private int TotalQuantity = 0;
+	static private int totalQuantity = 0;
 
 	// default constructor
 	TouristTour() {
 		this.quantity = 100;
 		this.discount = 30;
 		this.price = 300;
-		this.NameOfTourist = "Name Surname";
-		TotalQuantity += quantity;
+		this.nameOfTourist = "Name Surname";
+		totalQuantity += quantity;
 	}
 
 	// constructor with 4 parameters
-	TouristTour(int amount, int discount, double price, String NameOfTourist) {
+	TouristTour(int amount, int discount, double price, String nameOfTourist) {
 		this.quantity = amount;
 		this.discount = discount;
 		this.price = price;
-		this.NameOfTourist = NameOfTourist;
-		TotalQuantity += quantity;
+		this.nameOfTourist = nameOfTourist;
+		totalQuantity += quantity;
 	}
 
 	// constructor with 5 parameters
-	TouristTour(int quantity, int discount, double price, String NameOfTourist, boolean vip) {
+	TouristTour(int quantity, int discount, double price, String nameOfTourist, boolean vip) {
 		this.quantity = quantity;
 		this.discount = discount;
 		this.price = price;
-		this.NameOfTourist = NameOfTourist;
+		this.nameOfTourist = nameOfTourist;
 		this.vip = vip;
-		TotalQuantity += quantity;
+		totalQuantity += quantity;
 	}
 
 
@@ -43,27 +43,27 @@ public class TouristTour {
 
 	// method displays information about the object
 	public String toString() {
-		return "Name of tourist:" + this.NameOfTourist + ", discount:" + this.discount + ", Price:" + this.price
+		return "Name of tourist:" + this.nameOfTourist + ", discount:" + this.discount + ", Price:" + this.price
 				+ ", vip:" + this.vip + ", Quantity: " + this.quantity;
 	}
 
 	// method displays the value of the static field
 	public static void printStaticSum() {
-		System.out.println("Static field from method printStaticSum: " + TotalQuantity);
+		System.out.println("Static field from method printStaticSum: " + totalQuantity);
 	}
 
 	// method displays the value of the static field
 	public void printSum() {
-		System.out.println("Static field from method printSum: " + TotalQuantity);
+		System.out.println("Static field from method printSum: " + totalQuantity);
 	}
 
-	void resetValues(int quantity, int discount, double price, String NameOfTourist, boolean vip) {
-		TotalQuantity -= this.quantity;
-		TotalQuantity += quantity;
+	void resetValues(int quantity, int discount, double price, String nameOfTourist, boolean vip) {
+		totalQuantity -= this.quantity;
+		totalQuantity += quantity;
 		this.quantity = quantity;
 		this.discount = discount;
 		this.price = price;
-		this.NameOfTourist = NameOfTourist;
+		this.nameOfTourist = nameOfTourist;
 		this.vip = vip;
 
 	}
